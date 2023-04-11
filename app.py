e0729751640acd8edc7f58938a357d026bdf7237
OlgaDlzk <olga.dolzhko@gmail.com>
2023-04-11 16:49:31 -0400

    fixed

--- app.py
+++ app.py
@@ -3,65 +3,7 @@ from flask_pymongo import PyMongo
 import json
 from bson import json_util
 from bson.json_util import dumps
-# import create_mongodb
-import pymongo
-import pandas as pd
-import json
-
-def create_db():
-
-    # connection using pymongo
-    client = pymongo.MongoClient("mongodb://localhost:27017")
-
-    # read in csv file as dataframe
-    df_horizontal = pd.read_csv("static/etl/csv/complete_data/complete_horizontal.csv")
-    df_vertical = pd.read_csv("static/etl/csv/complete_data/complete_vertical.csv")
-
-    # map index to string for bson compatibility
-    df_horizontal.index = df_horizontal.index.map(str)
-    df_vertical.index = df_vertical.index.map(str)
-
-    # Nan to None for proper JSON specs
-    df_horizontal = df_horizontal.where(df_horizontal.notna(), None)
-    df_vertical = df_vertical.where(df_vertical.notna(), None)
-
-    # new variable of df that converts data to dictionary
-    # where each row is its own dictionary (orient="records")
-    # list of dictionaries
-    data_horizontal = df_horizontal.to_dict(orient="index")
-    data_vertical = df_vertical.to_dict(orient="index")
-
-    # connect to db, one will be created if it does not exist yet
-    db = client["streamTest"]
-
-    # connect to collection and drop if it exists
-    mycol = db["streamData"]
-    mycol.drop()
-
-    horCol = db["streamHorizontal"]
-    horCol.drop()
-
-    vertCol = db["streamVertical"]
-    vertCol.drop()
-
-    sunCol = db["streamSunburst"]
-    sunCol.drop()
-
-    # create streamData collection and insert data
-    # db.streamData.insert_many(data)
-    db.streamHorizontal.insert_one(data_horizontal)
-
-    db.streamVertical.insert_one(data_vertical)
-
-     #open sunburst json file
-    with open("static/etl/json/sunburst_data.json") as file:
-        file_data = json.load(file)
-
-    # insert into streamSunburst collection
-    if isinstance(file_data, list):
-        db.streamSunburst.insert_many(file_data)
-    else:
-        db.streamSunburst.insert_one(file_data)
+import create_mongodb
 
 # create instance of Flask class
 app = Flask(__name__)
@@ -74,7 +16,7 @@ mongo = PyMongo(app)
 @app.route("/")
 # what will be displayed on pg wrapped in this function
 def home():
-    create_db()
+    create_mongodb.create_db()
     # first document in the collection
     # first_record = mongo.db.streamHorizontal.find_one()
 
